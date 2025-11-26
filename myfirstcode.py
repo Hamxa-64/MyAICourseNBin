06d567b25d71d51e4b59dee1b830eb6ab28652d2
Hamxa-64 <mhamzashahid64@gmail.com>
2025-11-25 19:56:45 -0800

    my first code
    my first code with variable

--- myfirstcode.py
+++ myfirstcode.py
@@ -1,1 +1,14 @@
 print("Hello World")
+
+Bookid = 556
+print(Bookid)
+print(type(Bookid))
+
+Bookprice = 99.0
+print(Bookprice)
+print(type(Bookprice))
+
+Booktitle = "zerotwoone"
+print(Booktitle)
+print(type(Booktitle))
+
