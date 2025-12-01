277c1d207e742bdaf74a71ccb214cad1ea0e6e6f
Hamxa-64 <mhamzashahid64@gmail.com>
2025-12-01 20:54:37 +0500

    Sir clone file
    file clone of sir

---
+++ Case7ComparisonOperators.py
@@ -0,0 +1,21 @@
+a = 5
+
+b = 2
+
+# equal to operator
+print(' a == b =', a == b) #answer will be bool 2 int variable are compared
+
+# not equal to operator
+print('a != b =', a != b) #same answer bool
+
+# greater than operator
+print('a > b =', a > b) #same answer bool
+
+# less than operator
+print('a < b =', a < b) #same answer bool
+
+# greater than or equal to operator
+print('a >= b =', a >= b) 
+
+# less than or equal to operator
+print('a <= b =', a <= b)
