aa272c836814ed7df5331c62569e83e09e147274
Hamxa-64 <mhamzashahid64@gmail.com>
2025-12-01 20:24:24 +0500

    This is my sec code
    Hey this is my second code kindly check

---
+++ myseccode.py
@@ -0,0 +1,35 @@
+CityName = ("Lahore")
+CityLongitude = 47.677
+IsCapital = False
+CityZipCode = 467
+
+
+#print along with labels 
+print("CityName: ", CityName)
+print("CiyLongitude: ", CityLongitude)
+print("IsCapital:  ", IsCapital)
+print("CityZipCode: ", CityZipCode)
+
+#print along with concatenate 
+print("CityName: " + CityName)
+print("CityLongitude: " + str(CityLongitude))
+print("IsCapital: " + str(IsCapital))
+print("CityZipCode: " + str(CityZipCode))
+
+
+#print type 
+print(type("CityName"))
+print(type(CityLongitude))
+print(type(IsCapital))
+print(type(CityZipCode))
+
+
+Name = input("Please Enter your Name:  ")
+print("Hello - World - " + Name)
+
+
+Age = int(input("Please enter your Age:  "))
+print("your Age: ", Age)
+
+Height = float(input("Please enter your height:  "))
+print("your Height: ", Height)
