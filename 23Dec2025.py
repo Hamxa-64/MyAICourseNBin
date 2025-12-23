b2bacf3dfd7decd45ad591f32ad955875463af0a
Hamxa-64 <mhamzashahid64@gmail.com>
2025-12-23 20:02:21 +0500

    Data Structure
    This is my data structures file

---
+++ 23Dec2025.py
@@ -0,0 +1,22 @@
+Newspaperlist = [45, "Dawn", 25.5, True]
+print(Newspaperlist)
+print(type(Newspaperlist))
+print (Newspaperlist[1])
+print(type(Newspaperlist[1]))
+
+Newspaperlist.append("Hamza")
+print(Newspaperlist)
+
+Newspaperlist.insert(1,"Lahore")
+print(Newspaperlist)
+
+Newspaperlist.remove("Dawn")
+print(Newspaperlist)
+
+Newspaperlist.pop(4)
+print(Newspaperlist)
+
+
+
+
+
